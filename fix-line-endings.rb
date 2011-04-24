de07df484e19665148952766e92ad11cf0829f53
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-04-24 08:16:22 +0000

    Fixed the line endings and added a ruby script to do it automagically
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@125 3c8fc032-aa9e-0410-b36d-c4f24796240c

---
+++ fix-line-endings.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+files = Dir['**/*.c'] + Dir['**/*.h'] + Dir['**/*.tex']
+
+files.map do |file_name|
+  input = ''
+  File.open(file_name, 'r') do |file|
+    input = file.read
+  end
+  if /\r\n/ =~ input
+    puts "Fixing file [#{file_name}]"
+    File.open(file_name, 'w') do |file|
+      file.write(input.gsub(/\r\n/, "\n"))
+    end
+  end
+end
