faac5c9e97aa7512b3662266a693f737bfcbd867
scr52 <scr52@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-05-30 11:02:23 +0000

    Added LED implementation
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@275 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/led.c
+++ src/led.c
@@ -1,2 +1,76 @@
 #include "led.h"
 #include <avr/io.h>
+
+#define LED_VALUE_PINS ( _BV(1) | _BV(2) | _BV(3) | _BV(4) )
+#define LED_ENABLE_PIN _BV(6)
+#define LED_SELECT_PIN _BV(7)
+
+static struct {
+    uint8_t value;
+    uint8_t enabled;
+    uint8_t selected;
+} __left_display, __right_display;
+
+static enum { __L_D, __R_D } __current;
+
+void __callback() {
+    switch (__current) {
+        case __L_D:
+            __current = __R_D;
+            LED_PORT.OUT = __right_display.value
+                         | __right_display.enabled
+                         | __right_display.selected;
+            break;
+        case __R_D:
+            __current = __L_D;
+            LED_PORT.OUT = __left_display.value
+                         | __left_display.enabled
+                         | __left_display.selected;
+            break;
+    }
+}
+
+void led_init() {
+    // Set the pre-scaler to 1024.
+    // CLK freq is now 32 MHz / 1024 = 31250 Hz.
+    LED_TC.CTRLA = 0x07;
+
+    // Set the period of the interrupts.
+    // Period of 260 = 31250 / 260 ~= 120 Hz.
+    LED_TC.PER = 0x0104;
+
+    // Enable the interrupt and assign the callback.
+    // LED_TC.INTCTRLA = 0x01 ? 0x02 ? 0x03;
+
+    LED_PORT.DIRSET =  LED_VALUE_PINS
+                     | LED_ENABLE_PIN
+                     | LED_SELECT_PIN;
+
+    // Set the default values for the structs.
+    __left_display.value     = 0x00;
+    __left_display.enabled   = 0x00;
+    __left_display.selected  = LED_SELECT_PIN;
+    __right_display.value    = 0x00;
+    __right_display.enabled  = 0x00;
+    __right_display.selected = 0x00;
+}
+
+void led_display_left(uint8_t number) {
+    if (number < 0x10) {
+        __left_display.value   = number;
+        __left_display.enabled = LED_ENABLE_PIN;
+    } else {
+        __left_display.value   = 0x00;
+        __left_display.enabled = 0x00;
+    }
+}
+
+void led_display_right(uint8_t number) {
+    if (number < 0x10) {
+        __right_display.value   = number;
+        __right_display.enabled = LED_ENABLE_PIN;
+    } else {
+        __right_display.value   = 0x00;
+        __right_display.enabled = 0x00;
+    }
+}
--- src/led.h
+++ src/led.h
@@ -15,25 +15,29 @@
 void led_init();
 
 /**
- * Sets the numbers to display.
- *
- * @param left      The left-hand number to display.
- * @param right     The right-hand number to display.
- */
-void led_display(uint8_t, uint8_t);
-
-/**
- * Sets the number to display on the left.
+ * Sets the number to display on the left.  If it's greater than 0x0F then the
+ * left-hand display is turned off.
  *
  * @param number    The number to display.
  */
 void led_display_left(uint8_t);
 
 /**
- * Sets the number to display on the right.
+ * Sets the number to display on the right.  If it's greater than 0x0F then the
+ * right-hand display is turned off.
  *
  * @param number    The number to display.
  */
 void led_display_right(uint8_t);
+
+/**
+ * Displays an up to 16-bit number across both LEDs.
+ *
+ * @param number    The number to display.
+ */
+static inline void led_display(uint8_t number) {
+    led_display_left(number & 0x0F);
+    led_display_right(number >> 4);
+}
 
 #endif
