845302f80c0e40936322c57e5229e9449d0616a7
byu17 <byu17@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-05-31 11:07:23 +0000

    Checkpoint!
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@285 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- camera_hacking/hack.c
+++ camera_hacking/hack.c
@@ -13,7 +13,7 @@
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof (array[0]))
 
-#define MAX_MESSAGE_LENGTH 30
+#define MAX_MESSAGE_LENGTH 120
 
 /* Camera commands:
  * RESET: reset the camera device
@@ -54,7 +54,9 @@ int camera_reset(int serial_fd)
     serial_write(serial_fd, CAM_RESET_CMD, 4);
     serial_read(serial_fd, rx_buffer, 4);
 #ifdef DEBUG
+    fprintf(stderr, "camera_reset returned:\n");
     dump_contents(rx_buffer, 4);
+    fprintf(stderr, "\n");
 #endif
     
     return camera_check_retcode(rx_buffer, 4, CAM_RESET_RET);
@@ -68,7 +70,9 @@ int camera_start_image(int serial_fd)
     serial_write(serial_fd, CAM_STARTIMAGE_CMD, 5);
     serial_read(serial_fd, rx_buffer, 5);
 #ifdef DEBUG
+    fprintf(stderr, "camera_start_image returned:\n");
     dump_contents(rx_buffer, 5);
+    fprintf(stderr, "\n");
 #endif
     
     return camera_check_retcode(rx_buffer, 5, CAM_STARTIMAGE_RET);
@@ -84,7 +88,9 @@ int camera_stop_image(int serial_fd)
     serial_write(serial_fd, CAM_STOPIMAGE_CMD, 5);
     serial_read(serial_fd, rx_buffer, 5);  
 #ifdef DEBUG
+    fprintf(stderr, "camera_stop_image returned:\n");
     dump_contents(rx_buffer, 5);
+    fprintf(stderr, "\n");
 #endif
     
     return camera_check_retcode(rx_buffer, 5, CAM_STOPIMAGE_RET);
@@ -99,7 +105,9 @@ camera_get_filesize(int serial_fd)
     serial_write(serial_fd, CAM_SIZE_CMD, 5);
     serial_read(serial_fd, rx_buffer, 9);
 #ifdef DEBUG
+    fprintf(stderr, "camera_get_filesize returned:\n");
     dump_contents(rx_buffer, 9);
+    fprintf(stderr, "\n");
 #endif
     
     /* NOTE: Non-standard use of camera_check_retcode here. Only the first 7 
@@ -136,64 +144,59 @@ int camera_get_block(int fd, unsigned int *addr, unsigned int len,
     /* Get footer */
     serial_read(fd, cmd, 5);
 
-    addr += len;
+    *addr += len;
 
     return(0);
 }
 
+/* Must sleep for 2-3 seconds after camera power on. */
 int main(int argc, char *argv[])
 {
     char rx_buffer[MAX_MESSAGE_LENGTH];
-    memset(&rx_buffer, 0, MAX_MESSAGE_LENGTH);
+    memset(&rx_buffer, 3, MAX_MESSAGE_LENGTH);
 
-//     unsigned char *compressed_image = NULL;  // Buffer for compressed
-//     //compressed_image = (byte *) malloc(image_x_size * image_y_size * sizeof(color_type) * 2);
-//     compressed_image = (unsigned char *) malloc(640 * 480 * 1000 * 2);
-//         unsigned int block_size = 0xf8;
-    
+    unsigned char *compressed_image = NULL;
+    compressed_image = (unsigned char *) malloc(640 * 480 * 1000 * 2);
+    /* Default block size should be f8. */
+    unsigned int blocksize = 0xf8;
+    /* Default interval should be 0x0A */
+    unsigned char interval = 0x0a;
+    unsigned int addr = 0;
+
+    /* WARNING this is probably not setting the speed correctly */
     int serial_fd = serial_open("/dev/ttyU1", B38400);
-
-    
-    /* Must sleep for 2-3 seconds after camera power on. */
 
     camera_reset(serial_fd);
     usleep(1000000);
     camera_start_image(serial_fd);
     usleep(1000000);
-    camera_stop_image(serial_fd);
-    usleep(1000000);
 
-    
+
     uint16_t filesize = camera_get_filesize(serial_fd);
     fprintf(stderr, "DEBUG: filesize = %d\n", filesize);
 
-//     unsigned int addr = 0;
-//     uint8_t image_done = 0;
-//     uint8_t image_error = 0;
 
-//     while(!image_done)
-//     {
-//         unsigned int read_len = block_size;
-// 
-//         // The read size for the last block is smaller than the boock size
-//         if (read_len > filesize - addr)
-//             read_len = filesize - addr;
-// 
-//         /* Send read block command */
-//         if (camera_get_block(serial_fd, &addr, read_len, compressed_image + addr))
-//         {
-//             //add_line("Camera - Unable to transfer camera block");
-//             image_error = 1;
-//             break;
-//         }
-// 
-//         if (addr == filesize)
-//             image_done = 1;
-// 
-//     }
+    unsigned char cmd[] =  {0x56, 0x00, 0x32, 0x0C, 0x00, 0x0A, 0x00, 0x00,
+                            addr >> 8, addr, 0x00, 0x00,
+                            blocksize >> 8, blocksize, 0x00, interval};
+    while (1) {
+
+
+                            
+        serial_write(serial_fd, cmd, sizeof(cmd));
+        int bytes_rxed = serial_read(serial_fd, rx_buffer, MAX_MESSAGE_LENGTH);
+        addr += blocksize;
+        fprintf(stderr, "bytes_rxed: %d\n", bytes_rxed);
+        dump_contents(rx_buffer, bytes_rxed + 5);
+    //         fprintf(stderr, "cmd is: %s\n");
+        dump_contents(cmd, sizeof(cmd));
+        fprintf(stderr, "addr is %d\n", addr);
+    }
 
 
 
+    camera_stop_image(serial_fd);
+    usleep(1000000);
     
     close(serial_fd);
     
