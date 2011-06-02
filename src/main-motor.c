969ce0bdf6c5fa4743f8a681794b60b4972992cd
scr52 <scr52@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-02 06:35:58 +0000

    MOAR fsm
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@353 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/main-motor.c
+++ src/main-motor.c
@@ -34,16 +34,38 @@ int main(int argc, char *argv[]) {
     uint8_t num = 0;
 
     while(1) {
+        cmd_t latest_cmd, ir_cmd, bt_cmd;
+        ir_cmd = ir_get_cmd();
+        bt_cmd = CMD_NONE;// cam_get_cmd(); // TODO
+        if (controller == BT) {
+            if (ir_cmd == ASSUME_CTRL) {
+                controller = IR;
+                continue;
+            } else {
+                latest_cmd = bt_cmd;
+            }
+        } else {
+            if (bt_cmd == ASSUME_CTRL) {
+                controller = BT;
+                continue;
+            } else {
+                latest_cmd = ir_cmd;
+            }
+        }
+
         switch(controller) {
             case BT:
                 if (ir_get_cmd() == ASSUME_CTRL) {
                     controller = IR;
+                }
                 break;
             case IR:
                 if (blue_get_cmd() == ASSUME_CTRL) { //TODO: camera_board_get_cmd(); or something
                     controller = IR;
+                }
                 break;
-        }
+        /}
+
     }
 
     return 0;	
