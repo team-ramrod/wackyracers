fa36f511e5b3aa30f8cbf3bc149092f5ba47e58f
scr52 <scr52@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-09 12:54:08 +0000

    Added charger code and makefile config for it
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@331 3c8fc032-aa9e-0410-b36d-c4f24796240c

---
+++ src/charger.c
@@ -0,0 +1,35 @@
+/**
+ * @file   charger.c
+ * @author Henry Jenkins
+ * @date   20110609
+ */
+
+#include "charger.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define CHARGER_PORT        PORTB
+#define CHARGER_STATUS_PIN  _BV(2)
+
+static charger_callback __charger_callback = NULL;
+
+ISR(PORTB_INT0_vect) {
+    __charger_callback(charger_read());
+}
+
+void charger_init(){
+    /*
+     * set up interupt
+     */
+    CHARGER_PORT.INTCTRL = 0x02;
+    CHARGER_PORT.INT0MASK = _BV(2);
+}
+
+bool charger_read() {
+    return !(CHARGER_PORT.IN & CHARGER_STATUS_PIN);
+}
+
+void charger_set_callback(charger_callback callback) {
+    __charger_state = callback;
+}
+
