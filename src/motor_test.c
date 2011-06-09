e4f150bae0a4c7965e2b9dd68031eea331f9514a
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-09 12:45:51 +0000

    new motor test code
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@330 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_test.c
+++ src/motor_test.c
@@ -12,23 +12,21 @@
 #include <util/delay.h>
 
 int main(int argc, char *argv[]) {
-    uint8_t left = 0, right = 0;
+    uint8_t num = 0;
 
     clock_init();
     led_init();
     motor_init();
 
+
     while (1) {
-        led_display_left(left);
-        led_display_right(right);
+        led_display(num = (num + 1) % 100);
+        //motor_set_speed(LEFT, ((num - 1) % 2) * 20);
+        motor_set_speed(RIGHT, num * 5);
+        //motor_set_speed(LEFT,  left * 28);
+        //motor_set_speed(RIGHT, right * 28);
 
-        motor_set_speed(LEFT,  left * 28);
-        motor_set_speed(RIGHT, right * 28);
-
-        left  = rand() % 10;
-        right = rand() % 10;
-
-        _delay_ms(4000.0);
+        _delay_ms(12000.0);
     }
 
     return 0;
