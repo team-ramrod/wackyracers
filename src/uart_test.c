789d086b6e1ade8b2568fd1c995d1a943283136c
byu17 <byu17@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-10 03:06:18 +0000

    Some uart code
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@332 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/uart_test.c
+++ src/uart_test.c
@@ -1,184 +1,58 @@
-/* This file has been prepared for Doxygen automatic documentation generation.*/
-/*! \file *********************************************************************
- *
- * \brief  XMEGA USART interrupt driven driver example source.
- *
- *      This file contains an example application that demonstrates the
- *      interrupt driven USART driver. The code example sends three bytes, waits
- *      for three bytes to be received and tests if the received data equals the
- *      sent data.
- *
- * \par Application note:
- *      AVR1307: Using the XMEGA USART
- *
- * \par Documentation
- *      For comprehensive code documentation, supported compilers, compiler
- *      settings and supported devices see readme.html
- *
- * \author
- *      Atmel Corporation: http://www.atmel.com \n
- *      Support email: avr@atmel.com
- *
- * $Revision: 1694 $
- * $Date: 2008-07-29 14:21:58 +0200 (ti, 29 jul 2008) $  \n
- *
- * Copyright (c) 2008, Atmel Corporation All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. The name of ATMEL may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE EXPRESSLY AND
- * SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *****************************************************************************/
-#include "usart_driver.h"
-#include "avr_compiler.h"
-
-/*! Number of bytes to send in test example. */
-#define NUM_BYTES  3
-/*! Define that selects the Usart used in example. */
-#define USART USARTE0
-
-/*! USART data struct used in example. */
-USART_data_t USART_data;
-/*! Test data to send. */
-uint8_t sendArray[NUM_BYTES] = {0x55, 0xaa, 0xf0};
-/*! Array to put received data in. */
-uint8_t receiveArray[NUM_BYTES];
-/*! Success variable, used to test driver. */
-bool success;
-
-
-/*! \brief Example application.
- *
- *  Example application. This example configures USARTC0 for with the parameters:
- *      - 8 bit character size
- *      - No parity
- *      - 1 stop bit
- *      - 9600 Baud
- *
- *  This function then sends three bytes and tests if the received data is
- *  equal to the sent data. The code can be tested by connecting PC3 to PC2. If
- *  the variable 'success' is true at the end of the function, the three bytes
- *  have been successfully sent and received.
-*/
-int main(void)
+#include <stdio.h>
+#include <avr/io.h> 
+ 
+static int uart_putchar(char c, FILE *stream);
+static void uart_init (void);
+ 
+static FILE mystdout = FDEV_SETUP_STREAM (uart_putchar, NULL, _FDEV_SETUP_WRITE);
+ 
+ 
+int main (void)
 {
-	/* counter variable. */
-	uint8_t i;
-
-	/* This PORT setting is only valid to USARTC0 if other USARTs is used a
-	 * different PORT and/or pins are used. */
-  	/* PC3 (TXD0) as output. */
-	PORTE.DIRSET   = PIN3_bm;
-	/* PC2 (RXD0) as input. */
-	PORTE.DIRCLR   = PIN2_bm;
-
-	/* Use USARTC0 and initialize buffers. */
-	USART_InterruptDriver_Initialize(&USART_data, &USART, USART_DREINTLVL_LO_gc);
-
-	/* USARTC0, 8 Data bits, No Parity, 1 Stop bit. */
-	USART_Format_Set(USART_data.usart, USART_CHSIZE_8BIT_gc,
-                     USART_PMODE_DISABLED_gc, false);
-
-	/* Enable RXC interrupt. */
-	USART_RxdInterruptLevel_Set(USART_data.usart, USART_RXCINTLVL_LO_gc);
-
-	/* Set Baudrate to 9600 bps:
-	 * Use the default I/O clock frequency that is 2 MHz.
-	 * Do not use the baudrate scale factor
-	 *
-	 * Baudrate select = (1/(16*(((I/O clock frequency)/Baudrate)-1)
-	 *                 = 12
-	 */
-	USART_Baudrate_Set(&USART, 12 , 0);
-
-	/* Enable both RX and TX. */
-	USART_Rx_Enable(USART_data.usart);
-	USART_Tx_Enable(USART_data.usart);
-
-	/* Enable PMIC interrupt level low. */
-	PMIC.CTRL |= PMIC_LOLVLEX_bm;
-
-	/* Enable global interrupts. */
-	sei();
-
-        while(1){
-	/* Send sendArray. */
-	i = 0;
-	while (i < NUM_BYTES) {
-		bool byteToBuffer;
-		byteToBuffer = USART_TXBuffer_PutByte(&USART_data, sendArray[i]);
-		if(byteToBuffer){
-			i++;
-		}
-	}
-        }
-
-	/* Fetch received data as it is received. */
-	i = 0;
-	while (i < NUM_BYTES) {
-		if (USART_RXBufferData_Available(&USART_data)) {
-			receiveArray[i] = USART_RXBuffer_GetByte(&USART_data);
-			i++;
-		}
-	}
-
-	/* Test to see if sent data equals received data. */
-	/* Assume success first.*/
-	success = true;
-	for(i = 0; i < NUM_BYTES; i++) {
-		/* Check that each element is received correctly. */
-		if (receiveArray[i] != sendArray[i]) {
-			success = false;
-		}
-	}
-
-	/* If success the program ends up inside the if statement.*/
-	if(success){
-		while(true);
-	}else{
-	  	while(true);
-	}
+    uart_init();
+    stdout = &mystdout;
+ 
+    while (1)
+        printf("Hello, world!\n");
 }
-
-
-/*! \brief Receive complete interrupt service routine.
- *
- *  Receive complete interrupt service routine.
- *  Calls the common receive complete handler with pointer to the correct USART
- *  as argument.
- */
-ISR(USARTC0_RXC_vect)
+ 
+ 
+static int uart_putchar (char c, FILE *stream)
 {
-	USART_RXComplete(&USART_data);
+    if (c == '\n')
+        uart_putchar('\r', stream);
+ 
+    // Wait for the transmit buffer to be empty
+    while ( !( USARTE0.STATUS & USART_DREIF_bm) );
+ 
+    // Put our character into the transmit buffer
+    USARTE0.DATA = c; 
+ 
+    return 0;
 }
-
-
-/*! \brief Data register empty  interrupt service routine.
- *
- *  Data register empty  interrupt service routine.
- *  Calls the common data register empty complete handler with pointer to the
- *  correct USART as argument.
- */
-ISR(USARTC0_DRE_vect)
+ 
+ 
+// Init USART.  Transmit only (we're not receiving anything)
+// We use USARTC0, transmit pin on PC3.
+// Want 9600 baud. Have a 2 MHz clock. BSCALE = 0
+// BSEL = ( 2000000 / (2^0 * 16*9600)) -1 = 12
+// Fbaud = 2000000 / (2^0 * 16 * (12+1))  = 9615 bits/sec
+static void uart_init (void)
 {
-	USART_DataRegEmpty(&USART_data);
+    // Set the TxD pin high - set PORTC DIR register bit 3 to 1
+    PORTE.OUTSET = PIN3_bm;
+ 
+    // Set the TxD pin as an output - set PORTC OUT register bit 3 to 1
+    PORTE.DIRSET = PIN3_bm;
+ 
+    // Set baud rate & frame format
+    USARTE0.BAUDCTRLB = 0;                      // BSCALE = 0 as well
+    USARTE0.BAUDCTRLA = 51;
+ 
+    // Set mode of operation
+    USARTE0.CTRLA = 0;                          // no interrupts please
+    USARTE0.CTRLC = 0x03;                       // async, no parity, 8 bit data, 1 stop bit
+ 
+    // Enable transmitter only
+    USARTE0.CTRLB = USART_TXEN_bm;
 }
