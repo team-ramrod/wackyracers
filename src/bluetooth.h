9c1da03522f7a079383076a0fee804bccd52bdf8
zjt14 <zjt14@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-12 22:48:46 +0000

    finished uart interface to bluetooth
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@339 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/bluetooth.h
+++ src/bluetooth.h
@@ -7,7 +7,7 @@
 void blue_init(void);
 
 //reads the bluetooth and outputs the drive command to the motor board
-void blue_read_bluetooth (void);
+void blue_read_bluetooth (FILE *stdio_blue, FILE *stdio_motor);
 
 bool blue_send_byte(uint8_t byte);
 
--- src/main-cam.c
+++ src/main-cam.c
@@ -7,24 +7,44 @@
  *  Status: Untested
 */
 #include "common.h"
+#include "uart_cam_board.h"
 #include "bluetooth.h"
 #include "camera.h"
 #include "led.h"
+#include "clock.h"
 
+//sets up stdio for bluetooth
+FILE stdio_blue = FDEV_SETUP_STREAM (uart_putchar_blue, uart_getchar_blue, _FDEV_SETUP_RW);
 
+//sets up stdio for communicating with motor board
+FILE stdio_motor = FDEV_SETUP_STREAM (uart_putchar_motor, uart_getchar_motor, _FDEV_SETUP_RW);
+
+//sets up stdio for camera
+FILE stdio_cam = FDEV_SETUP_STREAM (uart_putchar_cam, uart_getchar_cam, _FDEV_SETUP_RW);
+
+char input;
+uint8_t num = 0;
 int main(int argc, char *argv[]) {
 //    set_sleep_state(A_SLEEP_STATE_THAT_WORKS);
-//    bluetooth_init();
 //    camera_init();
-//    sei();        
+        
     led_init();
+    clock_init();
+    uart_init_cam_board();
+    sei();
 
-    uint8_t num = 0;
+    
 
     while(1) {
-        blue_read_bluetooth();
         led_display(num++);
+        _delay_ms(500.0);
     }
 
     return 0;	
+}
+
+ISR(INTERRUPT_MOTOR)
+{
+    num = num+ 10;
+    blue_read_bluetooth(&stdio_blue, &stdio_motor);
 }
