6e0d43743eb0ef901d38dfbbf22429c7166955ea
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-23 12:17:13 +0000

    Better speeds
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@373 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_controller.c
+++ src/motor_controller.c
@@ -1,6 +1,9 @@
 #include "motor_controller.h"
 #include "motor.h"
 
+#define NORMAL_SPEED 30
+#define LOW_SPEED 10
+
 void motor_controller_init() {
     motor_init();
 }
@@ -10,50 +13,50 @@ void motor_set_movement(motor_vertical_t vert, motor_horizontal_t horiz) {
         case VERTICAL_FORWARD + HORIZONTAL_STOPPED:
             motor_set_direction(LEFT, FORWARD);
             motor_set_direction(RIGHT, FORWARD);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_BACKWARD + HORIZONTAL_STOPPED:
             motor_set_direction(LEFT, REVERSE);
             motor_set_direction(RIGHT, REVERSE);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_STOPPED + HORIZONTAL_LEFT:
             motor_set_direction(LEFT, REVERSE);
             motor_set_direction(RIGHT, FORWARD);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_FORWARD + HORIZONTAL_LEFT:
             motor_set_direction(LEFT, FORWARD);
             motor_set_direction(RIGHT, FORWARD);
-            motor_set_speed(LEFT, 1);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, LOW_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_BACKWARD + HORIZONTAL_LEFT:
             motor_set_direction(LEFT, REVERSE);
             motor_set_direction(RIGHT, REVERSE);
-            motor_set_speed(LEFT, 1);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, LOW_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_STOPPED + HORIZONTAL_RIGHT:
             motor_set_direction(LEFT, FORWARD);
             motor_set_direction(RIGHT, REVERSE);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 10);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, NORMAL_SPEED);
             break;
         case VERTICAL_FORWARD + HORIZONTAL_RIGHT:
             motor_set_direction(LEFT, FORWARD);
             motor_set_direction(RIGHT, FORWARD);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 1);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, LOW_SPEED);
             break;
         case VERTICAL_BACKWARD + HORIZONTAL_RIGHT:
             motor_set_direction(LEFT, REVERSE);
             motor_set_direction(RIGHT, REVERSE);
-            motor_set_speed(LEFT, 10);
-            motor_set_speed(RIGHT, 1);
+            motor_set_speed(LEFT, NORMAL_SPEED);
+            motor_set_speed(RIGHT, LOW_SPEED);
             break;
         case VERTICAL_STOPPED + HORIZONTAL_STOPPED:
         default:
