72621a13b498230f8736804a3d9343721b4dd79c
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-23 09:08:01 +0000

    Fixed Henrys blunder
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@369 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_controller.c
+++ src/motor_controller.c
@@ -7,10 +7,6 @@ void motor_controller_init() {
 
 void motor_set_movement(motor_vertical_t vert, motor_horizontal_t horiz) {
     switch(vert + horiz) {
-        case VERTICAL_STOPPED + HORIZONTAL_STOPPED:
-            motor_set_speed(LEFT, 0);
-            motor_set_speed(RIGHT, 0);
-            break;
         case VERTICAL_FORWARD + HORIZONTAL_STOPPED:
             motor_set_direction(LEFT, FORWARD);
             motor_set_direction(RIGHT, FORWARD);
