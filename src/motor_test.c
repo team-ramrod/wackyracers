bb5dcafc1676b6f4aece4dca39c773b840409d39
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-23 12:48:28 +0000

    Fully working motor test file
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@375 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_test.c
+++ src/motor_test.c
@@ -9,6 +9,11 @@
 #include "led.h"
 #include "motor_controller.h"
 
+ISR(BADISR_vect) {
+    led_display_left(0x00);
+    led_display_right(0x00);
+}
+
 int main(int argc, char *argv[]) {
     clock_init();
     led_init();
@@ -18,8 +23,44 @@ int main(int argc, char *argv[]) {
     uint8_t cmd = 0;
 
     while (1) {
-        fprintf(&stdio_to_motor,"asdf");
+        cmd = fgetc(&stdio_to_cam_board);
+        fprintf(&stdio_to_cam_board, "%c", cmd);
+        switch (cmd) {
+            case 's':
+            case '5':
+                motor_set_movement(VERTICAL_STOPPED, HORIZONTAL_STOPPED);
+                led_display(0);
+                break;
+            case 'w':
+            case '8':
+                motor_set_movement(VERTICAL_FORWARD, HORIZONTAL_STOPPED);
+                led_display(1);
+                break;
+            case 'x':
+            case '2':
+                motor_set_movement(VERTICAL_BACKWARD, HORIZONTAL_STOPPED);
+                led_display(2);
+                break;
+            case 'a':
+            case '4':
+                motor_set_movement(VERTICAL_STOPPED, HORIZONTAL_LEFT);
+                led_display(3);
+                break;
+            case 'd':
+            case '6':
+                motor_set_movement(VERTICAL_STOPPED, HORIZONTAL_RIGHT);
+                led_display(4);
+                break;
+            default:
+                led_display(cmd-60);
+                break;
+        }
     }
 
     return 0;
+}
+
+ISR(INTERRUPT_CAM)
+{
+    // Do nothing
 }
