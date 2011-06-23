ae4e669b3a4b7b06f28f8400f9a54d6fe5efe135
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-23 05:59:20 +0000

    The rest of the directions
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@363 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_controller.c
+++ src/motor_controller.c
@@ -17,5 +17,47 @@ void motor_set_movement(motor_vertical_t vert, motor_horizontal_t horiz) {
             motor_set_speed(LEFT, 10);
             motor_set_speed(RIGHT, 10);
             break;
+        case VERTICAL_BACKWARD + HORIZONTAL_STOPPED:
+            motor_set_direction(LEFT, REVERSE);
+            motor_set_direction(RIGHT, REVERSE);
+            motor_set_speed(LEFT, 10);
+            motor_set_speed(RIGHT, 10);
+            break;
+        case VERTICAL_STOPPED + HORIZONTAL_LEFT:
+            motor_set_direction(LEFT, REVERSE);
+            motor_set_direction(RIGHT, FORWARD);
+            motor_set_speed(LEFT, 10);
+            motor_set_speed(RIGHT, 10);
+            break;
+        case VERTICAL_FORWARD + HORIZONTAL_LEFT:
+            motor_set_direction(LEFT, FORWARD);
+            motor_set_direction(RIGHT, FORWARD);
+            motor_set_speed(LEFT, 1);
+            motor_set_speed(RIGHT, 10);
+            break;
+        case VERTICAL_BACKWARD + HORIZONTAL_LEFT:
+            motor_set_direction(LEFT, REVERSE);
+            motor_set_direction(RIGHT, REVERSE);
+            motor_set_speed(LEFT, 1);
+            motor_set_speed(RIGHT, 10);
+            break;
+        case VERTICAL_STOPPED + HORIZONTAL_RIGHT:
+            motor_set_direction(LEFT, FORWARD);
+            motor_set_direction(RIGHT, REVERSE);
+            motor_set_speed(LEFT, 10);
+            motor_set_speed(RIGHT, 10);
+            break;
+        case VERTICAL_FORWARD + HORIZONTAL_RIGHT:
+            motor_set_direction(LEFT, FORWARD);
+            motor_set_direction(RIGHT, FORWARD);
+            motor_set_speed(LEFT, 10);
+            motor_set_speed(RIGHT, 1);
+            break;
+        case VERTICAL_BACKWARD + HORIZONTAL_RIGHT:
+            motor_set_direction(LEFT, REVERSE);
+            motor_set_direction(RIGHT, REVERSE);
+            motor_set_speed(LEFT, 10);
+            motor_set_speed(RIGHT, 1);
+            break;
     }
 }
