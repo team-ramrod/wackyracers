771038b9dda216458ea06fa92282ef751ae1f32b
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-27 04:17:00 +0000

    Fixed debug output being intermingled
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@414 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/debug.c
+++ src/debug.c
@@ -1,13 +1,16 @@
 #include "debug.h"
 #include <stdarg.h>
+#include <util/atomic.h>
 
 void debug(char* verbosity, char* module, char* msg, ...) {
     va_list ap;
     va_start(ap, msg);
 
-    fprintf(stream_debug, "[%s] => %s:", module, verbosity);
-    vfprintf(stream_debug, msg, ap);
-    fprintf(stream_debug, "\n");
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        fprintf(stream_debug, "[%s] => %s:", module, verbosity);
+        vfprintf(stream_debug, msg, ap);
+        fprintf(stream_debug, "\n");
+    }
 
     va_end(ap);
 }
