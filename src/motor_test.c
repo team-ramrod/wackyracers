4ed047ea6b0d108206ed9505e46dc5e48c03986a
wgl18 <wgl18@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-27 04:18:05 +0000

    Latest testing code
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@417 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/motor_test.c
+++ src/motor_test.c
@@ -13,25 +13,29 @@
 #include <avr/interrupt.h>
 
 ISR(BADISR_vect) {
+    ERROR("motor_test", "Bad vector encountered, PMIC.STATUS = [%x]", PMIC.STATUS);
     led_display_left(0x00);
     led_display_right(0x00);
 }
 
 int main(int argc, char *argv[]) {
     clock_init();
+
+    uart_init();
+
     led_init();
-    button_init();
     motor_controller_init();
     commander_init();
-    interrupt_init();
 
-    button_wait_power();
+    interrupt_init();
 
     cmd_t cmd = 0;
 
     while (1) {
         cmd = get_cmd();
-        VERBOSE("motor_test", "Command recieved: [%i]", cmd);
+        if (cmd != CMD_NONE) {
+            VERBOSE("motor_test", "Command recieved: [%i]", cmd);
+        }
         switch (cmd) {
             case CMD_STOP:
                 motor_set_movement(VERT_STOPPED, HORIZ_STOPPED);
