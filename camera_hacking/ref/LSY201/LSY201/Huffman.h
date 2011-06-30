e18a1b0d8e7244f82835cf8591438da3a50038c7
byu17 <byu17@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-06-30 01:08:16 +0000

    Removed 40MB of crap from repos
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@435 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- camera_hacking/ref/LSY201/LSY201/Huffman.h
+++
@@ -1,9 +0,0 @@
-#ifndef HUFFMAN_H__
-#define HUFFMAN_H__
-
-extern const int gWTree[][2];
-extern const int gBTree[][2];
-extern const int gTwoTree[][2];
-extern const unsigned char gBackward[256];
-
-#endif	// HUFFMAN_H__
