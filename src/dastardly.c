783c73597ceb51298510c28dee31f2010dd4d61f
zjt14 <zjt14@3c8fc032-aa9e-0410-b36d-c4f24796240c>
2011-07-12 09:50:45 +0000

    stoped dasdidly interfering with camera
    git-svn-id: https://svn.elec.canterbury.ac.nz/courses/enel429-2011-wackyracers-9@490 3c8fc032-aa9e-0410-b36d-c4f24796240c

--- src/dastardly.c
+++ src/dastardly.c
@@ -6,7 +6,7 @@
 #include "debug.h"
 
 #define IR1_PWM DASTARDLY_IR1_TC.CCA
-#define IR2_PWM DASTARDLY_IR2_TC.CCB
+#define IR2_PWM DASTARDLY_IR2_TC.CCA
 
 void dastardly_init(){
     DEBUG("dastardly", "Started initialization.");
@@ -15,21 +15,21 @@ void dastardly_init(){
     DASTARDLY_IR1_TC.CTRLA = 0x01;
     DASTARDLY_IR2_TC.CTRLA = 0x01;
     
-    // Set PD5 pin required for PWM to be output.
-    PORTD.DIRSET = 0x10;
+    // Set PE0 pin required for PWM to be output.
+    PORTE.DIRSET = 0x01;
 
     // Set PC0 pin required for PWM to be output.
     PORTC.DIRSET = 0x01;
 
     // Set pwm output to override the pins
     // and set to single slope PWM mode.
-    DASTARDLY_IR1_TC.CTRLB = 0xF0 | 0x03;
-    DASTARDLY_IR2_TC.CTRLB = 0xF0 | 0x03;
+    DASTARDLY_IR1_TC.CTRLB = 0x10 | 0x03;
+    DASTARDLY_IR2_TC.CTRLB = 0x10 | 0x03;
 
     // Set the period of the output.
     // 8 kHz at 32 MHz clock = period of 4096 - 1 = 0x0999.
     DASTARDLY_IR1_TC.PER = 880;
-    DASTARDLY_IR2_TC.PER = 1100;
+    DASTARDLY_IR2_TC.PER = 1000;
 
     dastardly_ir_start();
 
